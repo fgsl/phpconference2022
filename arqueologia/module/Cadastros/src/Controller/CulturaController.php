32f8c9b17384862b43978bd621c5535f30c4bc6f
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2022-11-24 09:38:29 -0300

    Criação do módulo Cadastros com controlador e página inicial

---
+++ arqueologia/module/Cadastros/src/Controller/CulturaController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cadastros\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
+
+class CulturaController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+}
